d46982907b2461127446d8d9e81c76a900adabf1
Franco Valencia <franco.avalencia@gmail.com>
2026-08-11 12:27:16 -0600

    Cleaup old sdks

--- sdks/python/agent-composer-sdk/agent_composer_sdk/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A client library for accessing Agent Composer REST API"""
-
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- sdks/python/agent-composer-sdk/agent_composer_sdk/models/llm_provider.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class LLMProvider(str, Enum):
-    OPEN_AI = "open_ai"
-
-    def __str__(self) -> str:
-        return str(self.value)
